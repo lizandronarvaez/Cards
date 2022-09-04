6aa47357edcfa7036439d1e2508c397a3970ea11
Lizandro Narvaez <lizandrojesus13@hotmail.com>
2022-09-05 01:28:17 +0200

    Solucionando Errores

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -6,12 +6,7 @@ function Navegador() {
         <nav class="navbar navbar-expand-lg bg-black">
             <div class="container-fluid">
                 <h1 class="navbar-brand text-white">Lizandro Narvaez</h1>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <button class="btn btn-outline-success text-white" type="submit">Contacto</button>
-                </div>
+                <button class="btn btn-outline-success text-white" type="submit">Contacto</button>
             </div>
         </nav>
     )
